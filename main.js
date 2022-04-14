6c86d6b0fe387091b6abb03c6372857ca73daa01
Viktor <99023714+SkylionViktor@users.noreply.github.com>
2022-04-14 09:22:47 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const brg = document.querySelector('.menu_brg');
 const menu = document.querySelector('.menu_list');
-const wrapp = document.querySelector('.wrapper')
+const wrapp = document.querySelector('.wrapper');
 
 
-brg.addEventListener('click', ()=>{
+brg.addEventListener('click', function(){
    brg.classList.toggle('ative')
    menu.classList.toggle('visible')
    if(menu.classList.contains('visible')){
       wrapp.style.overflowY = 'hidden';
    }else{
       wrapp.style.overflowY = 'auto';
-   }
-})
+   };
+});
